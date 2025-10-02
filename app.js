b64586bcd999f8ca66608ed8a174edbbd7d6b491
Swapnaja14 <swapnajayadav2021@gmail.com>
2025-10-03 02:25:55 +0530

    Create RESTful APIs

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing');
 const path = require("path");
 const ejs = require('ejs');
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/roamnest";
 
@@ -54,21 +56,45 @@ app.get("/listings", async (req, res) => {
 });
 
 // New Route
+// GET /listings/new --> We get a Form
+// POST /listings --> Submit
 app.get("/listings/new", (req, res) => {
-    res.render("listings/new");
-});
-
-// Create Route
-app.post("/listings", async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save(); 
-    console.log(newListing);
-    res.redirect("/listings");
+    res.render("listings/new.ejs");
 });
 
 // Show Route
 app.get("/listings/:id", async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/show", {listing});
+    res.render("listings/show.ejs", {listing});
+});
+
+
+// Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+// Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
+
+// Update Route
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body});
+    res.redirect("/listings");
+});
+
+// DELETE LISTING
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
 });
